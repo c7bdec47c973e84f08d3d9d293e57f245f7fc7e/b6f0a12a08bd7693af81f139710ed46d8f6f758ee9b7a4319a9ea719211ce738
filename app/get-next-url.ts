6c13be0372e0d9f4baf5011d467d62d434d7b0f9
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-14 21:15:00 -0700

    Skip and go to dashboard

--- app/get-next-url.ts
+++ app/get-next-url.ts
@@ -1,10 +1,13 @@
-export function getNextUrl(): string | null {
+export function getNextUrl() {
   if (
     process.env.GET_STARTED_TEAM_ID &&
     process.env.GET_STARTED_PROJECT_NAME &&
     process.env.GET_STARTED_ORIGIN
   ) {
-    return `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}`;
+    return {
+      openPullRequest: `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}`,
+      openDashboard: `${process.env.GET_STARTED_ORIGIN}/${process.env.GET_STARTED_TEAM_ID}/${process.env.GET_STARTED_PROJECT_NAME}`,
+    };
   }
   return null;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,24 @@ export default function Home() {
           )}
         </div>
         {nextUrl && (
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <div className="flex flex-col gap-4 items-center sm:items-start">
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <a
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                href={nextUrl.openPullRequest}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Create a pull request
+              </a>
+            </div>
             <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href={nextUrl}
+              className="font-[family-name:var(--font-geist-mono)] text-sm text-gray-500 hover:underline"
+              href={nextUrl.openDashboard}
               target="_blank"
               rel="noopener noreferrer"
             >
-              Create a pull request
+              Skip and go to dashboard
             </a>
           </div>
         )}
