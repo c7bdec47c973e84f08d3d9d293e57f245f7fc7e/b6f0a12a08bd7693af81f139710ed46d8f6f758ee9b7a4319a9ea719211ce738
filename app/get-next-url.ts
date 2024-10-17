f322989ba079c143de03b2b8742f906338f4d26d
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-16 22:06:47 -0700

    Refactor

--- app/get-next-url.ts
+++ app/get-next-url.ts
@@ -1,12 +1,12 @@
-export function getNextUrl() {
+export function getNextUrl(step: string) {
   if (
     process.env.GET_STARTED_TEAM_ID &&
     process.env.GET_STARTED_PROJECT_NAME &&
     process.env.GET_STARTED_ORIGIN
   ) {
     return {
-      openPullRequest: `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}`,
-      openDashboard: `${process.env.GET_STARTED_ORIGIN}/${process.env.GET_STARTED_TEAM_ID}/${process.env.GET_STARTED_PROJECT_NAME}`,
+      next: `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}&step=${step}`,
+      skip: `${process.env.GET_STARTED_ORIGIN}/${process.env.GET_STARTED_TEAM_ID}/${process.env.GET_STARTED_PROJECT_NAME}`,
     };
   }
   return null;
