08c4b430098beca4a22a4f9b525b0510abe78a14
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-17 16:47:31 -0700

    Use stepName

--- app/get-next-url.ts
+++ app/get-next-url.ts
@@ -1,11 +1,11 @@
-export function getNextUrl(step: string) {
+export function getNextUrl(stepName: string) {
   if (
     process.env.GET_STARTED_TEAM_ID &&
     process.env.GET_STARTED_PROJECT_NAME &&
     process.env.GET_STARTED_ORIGIN
   ) {
     return {
-      next: `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}&step=${step}`,
+      next: `${process.env.GET_STARTED_ORIGIN}/new/starter-kit/step?teamId=${process.env.GET_STARTED_TEAM_ID}&projectName=${process.env.GET_STARTED_PROJECT_NAME}&stepName=${stepName}`,
       skip: `${process.env.GET_STARTED_ORIGIN}/${process.env.GET_STARTED_TEAM_ID}/${process.env.GET_STARTED_PROJECT_NAME}`,
     };
   }
