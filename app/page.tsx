be978361b013d4fcf93974803baf5a299def5cff
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-18 23:58:29 -0700

    Update instructions

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,52 @@
 import Image from "next/image";
 import { getNextUrl } from "./get-next-url";
+import bubbleSvg from "./bubble.svg";
 
 export default function Home() {
   const nextUrl = getNextUrl("lowercase-letters");
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-md">
-        <Image
-          className="invert dark:invert-0"
-          src="/vercel.svg"
-          alt="Vercel logo"
-          width={50}
-          height={50}
-          priority
-        />
-        <div className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] leading-relaxed">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-md text-sm font-[family-name:var(--font-geist-mono)] ">
+        <div className="flex items-center gap-4">
+          <Image
+            className="invert dark:invert-0"
+            src="/vercel.svg"
+            alt="Vercel logo"
+            width={50}
+            height={50}
+            priority
+          />
+          <div className="text-gray-500 hidden sm:block">
+            ← Comment “Let&apos;s make this smaller” here
+          </div>
+        </div>
+        <div className="text-center sm:text-left leading-relaxed">
           <p className="mb-4">
-            Congratulations! You&apos;ve successfully created your first preview
-            deployment for the Vercel Starter Kit — by creating and pushing to a
-            new branch.
+            Congratulations! You&apos;ve created your first preview deployment
+            for the Vercel Starter Kit — by creating and pushing to a new
+            branch.
           </p>
           <p className="mb-4">
             Vercel lets you collaborate with others by commenting directly on a
             preview deployment.
           </p>
           <p className="mb-4">
-            For example, TRY SELECTING THIS TEXT and add a comment: “Let&apos;s
-            use lowercase letters here.”
+            First, press &ldquo;C&rdquo; on your keyboard. Your cursor will
+            change to{" "}
+            <Image
+              src={bubbleSvg}
+              alt="the plus icon"
+              className="inline-flex w-6"
+            />
+            . Then, click on the triangle logo at the top and add a comment:
+            “Let&apos;s make this smaller”.
           </p>
           {nextUrl && (
             <p>
-              Next, let&apos;s push a commit to fix the above issue. By clicking
-              the button below, the Vercel bot will push a commit to use
-              lowercase letters.
+              Next, let&apos;s push a commit to make this happen. By clicking
+              the button below, the Vercel bot will push a commit to make the
+              triangle logo smaller.
             </p>
           )}
         </div>
@@ -41,7 +54,7 @@ export default function Home() {
           <div className="flex flex-col gap-4 items-center sm:items-start">
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <a
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-geist-sans)]"
                 href={nextUrl.next}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -50,7 +63,7 @@ export default function Home() {
               </a>
             </div>
             <a
-              className="font-[family-name:var(--font-geist-mono)] text-sm text-gray-500 hover:underline"
+              className="font-[family-name:var(--font-geist-mono)] text-gray-500 hover:underline"
               href={nextUrl.skip}
               target="_blank"
               rel="noopener noreferrer"
