ef0d6ed5a3b4b3b2ba0d4ff9b5a505c9f8fd4024
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-19 00:00:46 -0700

    update css

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   const nextUrl = getNextUrl("first-preview-deployment");
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-md">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-md text-sm font-[family-name:var(--font-geist-mono)]">
         <Image
           className="invert dark:invert-0"
           src="/vercel.svg"
@@ -15,7 +15,7 @@ export default function Home() {
           height={50}
           priority
         />
-        <div className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] leading-relaxed">
+        <div className="text-center sm:text-left font-[family-name:var(--font-geist-mono)] leading-relaxed">
           <p className="mb-4">
             Congratulations! You&apos;ve successfully deployed the Vercel
             Starter Kit.
@@ -32,7 +32,7 @@ export default function Home() {
           <div className="flex flex-col gap-4 items-center sm:items-start">
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <a
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc]  sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-geist-sans)]"
                 href={nextUrl.next}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -41,7 +41,7 @@ export default function Home() {
               </a>
             </div>
             <a
-              className="font-[family-name:var(--font-geist-mono)] text-sm text-gray-500 hover:underline"
+              className="font-[family-name:var(--font-geist-mono)] text-gray-500 hover:underline"
               href={nextUrl.skip}
               target="_blank"
               rel="noopener noreferrer"
