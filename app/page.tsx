4c45ce7aa8bc6230965140f52f60218eb235ae30
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-19 21:23:13 -0700

    Fix nextUrl

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
-import { getNextUrl } from "./get-next-url";
+import { useState, useEffect } from "react";
 import bubbleSvg from "./bubble.svg";
 
 export default function Home() {
-  const nextUrl = getNextUrl("lowercase-letters");
+  const stepName = "lowercase-letters";
+  const [nextUrlPrefix, setNextUrlPrefix] = useState<string>("");
+
+  useEffect(() => {
+    const host = window.location.host;
+
+    if (host !== "localhost") {
+      setNextUrlPrefix(
+        `https://vercel-site-git-chibicode-life-4346-create-a-demo-for-de-c94e2a.vercel.sh/new/starter-kit/step?origin=${encodeURIComponent(
+          window.location.origin,
+        )}&stepName=`,
+      );
+    }
+  }, []);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -39,7 +52,7 @@ export default function Home() {
             . Then, click on the triangle logo at the top and add a comment:
             “Let&apos;s make this smaller”.
           </p>
-          {nextUrl && (
+          {nextUrlPrefix && (
             <p>
               Next, let&apos;s push a commit to make this happen. By clicking
               the button below, the Vercel bot will push a commit to make the
@@ -47,12 +60,12 @@ export default function Home() {
             </p>
           )}
         </div>
-        {nextUrl && (
+        {nextUrlPrefix && (
           <div className="flex flex-col gap-4 items-center sm:items-start">
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <a
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-geist-sans)]"
-                href={nextUrl.next}
+                href={`${nextUrlPrefix}${stepName}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -61,7 +74,7 @@ export default function Home() {
             </div>
             <a
               className="font-[family-name:var(--font-geist-mono)] text-gray-500 hover:underline"
-              href={nextUrl.skip}
+              href={`${nextUrlPrefix}skip`}
               target="_blank"
               rel="noopener noreferrer"
             >
