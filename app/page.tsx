d7cfd3e738e7c76a6ce659b1250137577d986bcf
c7bdec47c973e84f08d3d9d293e57f245f7fc7e <166485857+c7bdec47c973e84f08d3d9d293e57f245f7fc7e@users.noreply.github.com>
2024-10-20 23:06:21 -0700

    Remove check

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const stepName = "first-preview-deployment";
-  const [nextUrlPrefix, setNextUrlPrefix] = useState<string>("");
+  const [nextUrlPrefix, setNextUrlPrefix] = useState<string>("#");
 
   useEffect(() => {
     const host = window.location.host;
@@ -35,36 +35,32 @@ export default function Home() {
             Congratulations! You&apos;ve successfully deployed the Vercel
             Starter Kit.
           </p>
-          {nextUrlPrefix && (
-            <p>
-              Next, let&apos;s create a pull request to improve this page. By
-              clicking the button below, the Vercel bot will create a pull
-              request for you.
-            </p>
-          )}
+          <p>
+            Next, let&apos;s create a pull request to improve this page. By
+            clicking the button below, the Vercel bot will create a pull request
+            for you.
+          </p>
         </div>
-        {nextUrlPrefix && (
-          <div className="flex flex-col gap-4 items-center sm:items-start">
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
-              <a
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-geist-sans)]"
-                href={`${nextUrlPrefix}${stepName}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Create a pull request
-              </a>
-            </div>
+        <div className="flex flex-col gap-4 items-center sm:items-start">
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
-              className="font-[family-name:var(--font-geist-mono)] text-gray-500 hover:underline"
-              href={`${nextUrlPrefix}skip`}
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-geist-sans)]"
+              href={`${nextUrlPrefix}${stepName}`}
               target="_blank"
               rel="noopener noreferrer"
             >
-              Skip and go to dashboard
+              Create a pull request
             </a>
           </div>
-        )}
+          <a
+            className="font-[family-name:var(--font-geist-mono)] text-gray-500 hover:underline"
+            href={`${nextUrlPrefix}skip`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Skip and go to dashboard
+          </a>
+        </div>
       </main>
     </div>
   );
